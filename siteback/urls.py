f7eca97262388ff004726bb86522a8f0a4ad4a23
Saad Zain <copyhaitu420@gmail.com>
2024-09-22 11:00:47 +0500

    commit

--- siteback/urls.py
+++ siteback/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from contact.views import IndexView
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,7 @@ urlpatterns = [
     path('api/fact', include('factcard.urls')),
     path('api/review', include('review.urls')),
     path('', IndexView.as_view(), name='home'),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
